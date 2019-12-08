ea1bb415ba353b28cf924b7a7b3f8701c4d37516
Robert Hilton <robert.a.hilton.jr@gmail.com>
2019-12-08 17:53:41 -0500

    Change menu preheat option to PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1446,9 +1446,9 @@
 #define PREHEAT_1_TEMP_BED     45
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED      0
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 235
+#define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
